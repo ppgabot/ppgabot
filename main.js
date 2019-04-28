0a6bc7d1e981f576dd6b0f8dfa79ce57ed397910
ppgabot <49957486+ppgabot@users.noreply.github.com>
2019-04-28 13:09:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const prefix = ";"
 
 Client.on("ready", () => {
 	console.log("online");
-	Client.user.setPresence({ game: { name: `;commandes | TOUR EIFFEL`, type: 0} });
+	Client.user.setPresence({ game: { name: `;commandes | RIP`, type: 0} });
 });
 
 
